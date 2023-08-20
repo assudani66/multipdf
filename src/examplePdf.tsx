7529d4989f8c2366ae831797cad98fcd68775b18
Piyush Assudani <63794264+assudani66@users.noreply.github.com>
2023-08-20 15:41:35 +0530

    fix src route

--- src/examplePdf.tsx
+++ src/examplePdf.tsx
@@ -1,8 +1,8 @@
 const ExampleReactPDF = () => {
     return (
         <div>
-            <iframe src="../piyush_resume.pdf" style={{ width: "600px", height: "500px" }}></iframe>
-            <iframe src="../piyush_resume.pdf" style={{ width: "600px", height: "500px" }}></iframe>
+            <iframe src="piyush_resume.pdf" style={{ width: "600px", height: "500px" }}></iframe>
+            <iframe src="piyush_resume.pdf" style={{ width: "600px", height: "500px" }}></iframe>
         </div>
     )
 };
